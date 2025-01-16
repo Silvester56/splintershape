60cb44d53b767c96fdeadc3caa716820a87afcf4
silvester <silvester>
2025-01-16 12:30:08 +0100

    Make enemy vision rotate when the enemy change direction

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var vision: Node = get_node("Vision")
 const SPEED = 100.0
 var directionX = 1
 
@@ -7,3 +8,4 @@ func _physics_process(delta: float) -> void:
 	velocity.x = directionX * SPEED
 	if move_and_slide():
 		directionX = -directionX
+		vision.rotation = vision.rotation + deg_to_rad(180)
