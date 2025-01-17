35713fccc52e6ab78fe03cae146106a77f990a8a
silvester <silvester>
2025-01-17 17:12:20 +0100

    Add behaviors to enemies

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,25 +1,33 @@
 extends CharacterBody2D
 
-var currentShape = Shape.RED_SQUARE
-
 enum EnemyState {
 	IDLE,
-	HORIZONTAL_PATROL,
-	VERTICAL_PATROL,
+	PATROL,
+	ROTATE,
 	SLEEPING
 }
 
+@export var currentState = EnemyState.PATROL
+@export var directionX = 0
+@export var directionY = 0
+@export var startingRotation = 0
+var currentShape = Shape.RED_SQUARE
 var isEnemy = true
-var currentState = EnemyState.HORIZONTAL_PATROL
 const SPEED = 100.0
-var directionX = 1
+
+func _ready() -> void:
+	$Vision.rotation = deg_to_rad(startingRotation)
 
 func _physics_process(delta: float) -> void:
-	if currentState == EnemyState.HORIZONTAL_PATROL:
+	if currentState == EnemyState.PATROL:
 		velocity.x = directionX * SPEED
+		velocity.y = directionY * SPEED
 		if move_and_slide():
 			directionX = -directionX
+			directionY = -directionY
 			$Vision.rotation = $Vision.rotation + deg_to_rad(180)
+	elif currentState == EnemyState.ROTATE:
+		$Vision.rotation = $Vision.rotation + 0.05
 	elif currentState == EnemyState.SLEEPING:
 		$Vision.visible = false
 
