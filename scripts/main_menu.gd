edc38f799e75bfe54edc1ac86d2505555d52a466
silvester <silvester>
2025-01-18 17:54:06 +0100

    Add credits

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -8,3 +8,6 @@ func _on_options_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits_menu.tscn")
