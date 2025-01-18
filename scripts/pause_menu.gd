d70129b9775f27fd8bf3fd4c0a772b9e3bc50c9f
silvester <silvester>
2025-01-18 18:04:33 +0100

    Imporve menus

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -3,5 +3,6 @@ extends Control
 func _on_resume_pressed() -> void:
 	$"../../..".pause()
 
-func _on_quit_pressed() -> void:
-	get_tree().quit()
+func _on_to_main_menu_pressed() -> void:
+	$"../../..".pause()
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
